9d2f9ddc244e4d69b10aff1c4f54ca3d48591d2a
Vikas Goyal <vikasgoyalgzs@gmail.com>
2015-06-30 18:28:18 -0400

    Code reorganised

--- src/services/trades.js
+++ src/services/trades.js
@@ -15,62 +15,20 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var __param = (this && this.__param) || function (paramIndex, decorator) {
     return function (target, key) { decorator(target, key, paramIndex); }
 };
-/// <reference path="../jspm_packages/npm/angular2@2.0.0-alpha.28/angular2.d.ts" />
-var angular2_1 = require('jspm_packages/npm/angular2@2.0.0-alpha.28/angular2');
-var http_1 = require('jspm_packages/npm/angular2@2.0.0-alpha.28/http');
+/// <reference path="../../jspm_packages/npm/angular2@2.0.0-alpha.28/angular2.d.ts" />
+var angular2_d_1 = require('../../jspm_packages/npm/angular2@2.0.0-alpha.28/angular2.d');
+var http_d_1 = require('../../jspm_packages/npm/angular2@2.0.0-alpha.28/http.d');
 var Trades = (function () {
     function Trades(_http) {
         this.http = _http;
-        //this.trades = [{
-        //  "id": 0,
-        //  "creationTime": "2014-12-22T02:21:58 +05:00",
-        //  "side": "Buy",
-        //  "symbol": "ORCL",
-        //  "quantity": 3907,
-        //  "placed": 2640,
-        //  "executed": 744,
-        //  "limitPrice": 2607,
-        //  "priority": 72,
-        //  "status": "Executed",
-        //  "trader": "ipsum"
-        //},
-        //  {
-        //    "id": 1,
-        //    "creationTime": "2015-03-06T23:59:56 +05:00",
-        //    "side": "Sell",
-        //    "symbol": "AAPL",
-        //    "quantity": 72,
-        //    "placed": 2658,
-        //    "executed": 635,
-        //    "limitPrice": 3048,
-        //    "priority": 27,
-        //    "status": "Executed",
-        //    "trader": "et"
-        //  },
-        //  {
-        //    "id": 2,
-        //    "creationTime": "2014-07-17T20:33:55 +04:00",
-        //    "side": "Buy",
-        //    "symbol": "AAPL",
-        //    "quantity": 1602,
-        //    "placed": 808,
-        //    "executed": 373,
-        //    "limitPrice": 1465,
-        //    "priority": 19,
-        //    "status": "New",
-        //    "trader": "incididunt"
-        //  }];
-        ////http.get('mocks/trades/trades_GET.json').subscribe(res => this.trades = res.text);
     }
     Trades.prototype.get = function () {
         return this.http('mocks/trades/trades_GET.json')
             .map(function (res) { return res.json(); });
     };
-    Trades.prototype.set = function () {
-    };
     Trades = __decorate([
-        angular2_1.Injectable(),
-        __param(0, angular2_1.Inject(http_1.HttpFactory)), 
+        angular2_d_1.Injectable(),
+        __param(0, angular2_d_1.Inject(http_d_1.HttpFactory)), 
         __metadata('design:paramtypes', [(typeof IHttp !== 'undefined' && IHttp) || Object])
     ], Trades);
     return Trades;
