45b67ca20bd73d699e049ed096a80cb2dad91f32
Vikas Goyal <vikasgoyalgzs@gmail.com>
2015-06-30 18:58:20 -0400

    Restored login file

---
+++ src/components/login/login.ts
@@ -0,0 +1,24 @@
+/// <reference path="../../../jspm_packages/npm/angular2@2.0.0-alpha.28/angular2.d.ts" />
+
+import {Component, View, Injector} from 'jspm_packages/npm/angular2@2.0.0-alpha.28/angular2';
+import {Router} from 'jspm_packages/npm/angular2@2.0.0-alpha.28/router';
+
+@Component({
+  selector: 'login',
+  appInjector: [Router]
+})
+@View({
+  templateUrl: 'components/login/login.html'
+})
+export class Login {
+  router: Router;
+
+  constructor (r: Router) {
+    this.router = r;
+  }
+  login (user) {
+    //router.navigate('/Home/' + '0');
+    //console.log(userName);
+  }
+}
+
